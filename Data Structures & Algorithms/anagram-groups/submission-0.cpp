11e29ac8c6ddb1a94cecb1d52d9c706b7365d4bc
tranphuongtrinhstg <tranphuongtrinhstg159@gmail.com>
2026-07-05 15:46:01 +0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        map<char, int> m;
+        for(int i = 0 ; i < s.size(); i++){
+            if(m.find(s[i]) == m.end()){ // khong tim thay 
+                m[s[i]] = 1;
+            } else{
+                m[s[i]]++;
+            }
+        }
+
+        for(int i = 0 ; i < t.size(); i++){
+            if(m.find(t[i]) == m.end()){
+                return false;
+            }else{
+                m[t[i]]--;
+                if(m[t[i]] == 0){
+                    m.erase(t[i]);
+                }
+            }
+        }
+        return m.empty();
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        int k = -1;
+        vector<bool> visited(strs.size(), false);
+        for(int i = 0 ; i < strs.size(); i++){
+            if(visited[i] == true) continue;
+            ans.push_back({strs[i]});
+            k++;
+            for(int j = i+1 ; j < strs.size(); j++){
+                if(visited[j] == false){
+                    if(isAnagram(strs[i], strs[j])){
+                        visited[j] = true;
+                        ans[k].push_back(strs[j]);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
